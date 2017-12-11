621337f792520a936adf675e8eaefa697c7a72ff
tomas.dusik <tomas.dusik@ibillboard.com>
2017-12-11 22:28:49 +0100

    removed useless tests

--- src/app/rules/branch-meets-all-prerequisites.spec.ts
+++ src/app/rules/branch-meets-all-prerequisites.spec.ts
@@ -1,6 +1,5 @@
 import {BranchMeetsAllPrerequisites} from "./branch-meets-all-prerequisites";
 
 describe('BranchMeetsAllPrerequisites', () => {
-    const branchMeetsAllPrerequisites: BranchMeetsAllPrerequisites = new BranchMeetsAllPrerequisites();
 
 });
